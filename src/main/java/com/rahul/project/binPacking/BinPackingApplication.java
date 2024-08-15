a9f0fda5dc0a089901dcd788093856dce3d5cbf7
RahulGattu23 <85167478+RahulGattu23@users.noreply.github.com>
2024-08-15 19:36:48 +0530

    Add files via upload

---
+++ src/main/java/com/rahul/project/binPacking/BinPackingApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.project.binPacking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinPackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinPackingApplication.class, args);
+	}
+
+}
