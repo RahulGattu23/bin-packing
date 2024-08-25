15849a10f7ce847afc56fec83c403835b8d89508
RahulGattu23 <85167478+RahulGattu23@users.noreply.github.com>
2024-08-25 15:43:57 +0530

    Delete service directory

--- service/strategy/AssortmentStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.rahul.project.binPacking.service.strategy;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public abstract class AssortmentStrategy {
-    public abstract long compute();
-}
